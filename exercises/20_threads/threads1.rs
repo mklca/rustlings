05eec03e9cac4877e64d087b1b266d0c5fc5dc0b
Mikaela Allan <me@mikaela.ca>
2023-10-25 13:49:24 -0400

    completed exercises 20_threads

--- exercises/20_threads/threads1.rs
+++ exercises/20_threads/threads1.rs
@@ -8,8 +8,7 @@
 // Execute `rustlings hint threads1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
+use std::panic::resume_unwind;
 use std::thread;
 use std::time::{Duration, Instant};
 
@@ -25,8 +24,11 @@ fn main() {
     }
 
     let mut results: Vec<u128> = vec![];
-    for handle in handles {
-        // TODO: a struct is returned from thread::spawn, can you use it?
+    for (i, handle) in handles.into_iter().enumerate() {
+        match handle.join() {
+            Ok(result) => results.push(result),
+            Err(err) => resume_unwind(err)
+        }
     }
 
     if results.len() != 10 {
--- exercises/20_threads/threads2.rs
+++ exercises/20_threads/threads2.rs
@@ -7,9 +7,8 @@
 // Execute `rustlings hint threads2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-use std::sync::Arc;
+use std::ops::{Deref, DerefMut};
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -18,22 +17,28 @@ struct JobStatus {
 }
 
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
+    let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));
     let mut handles = vec![];
-    for _ in 0..10 {
+    for i in 0..10 {
         let status_shared = Arc::clone(&status);
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
-            // TODO: You must take an action before you update a shared value
-            status_shared.jobs_completed += 1;
+            match status_shared.lock() {
+                Ok(mut status_guard) => {
+                    status_guard.deref_mut().jobs_completed += 1;
+                },
+                Err(mutex_error) => panic!("thread {} failed to lock mutex: {}", i, mutex_error)
+            }
         });
         handles.push(handle);
     }
     for handle in handles {
         handle.join().unwrap();
-        // TODO: Print the value of the JobStatus.jobs_completed. Did you notice
-        // anything interesting in the output? Do you have to 'join' on all the
-        // handles?
-        println!("jobs completed {}", ???);
+        match status.lock() {
+            Ok(status_guard) => {
+                println!("jobs completed {}", status_guard.deref().jobs_completed);
+            },
+            Err(mutex_error) => panic!("main thread failed to lock mutex: {}", mutex_error)
+        }
     }
 }
